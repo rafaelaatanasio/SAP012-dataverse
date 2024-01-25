89a182503d27505e35dd05c0984ba5f42c26208d
deboraviveiros <deviveiros.debora@gmail.com>
2024-01-25 13:59:55 -0300

    Inserções no html e no view.js

--- src/view.js
+++ src/view.js
@@ -1,5 +1,9 @@
 export const renderItems = (data) => {
   console.log(data)
+  // < div itemscope >
+  //   <ul>
+  //     <li></li>
+  //   </ul >
   // Aquí comienza tu código y puedes retornar lo que tu necesites
   return 'example';
 };
