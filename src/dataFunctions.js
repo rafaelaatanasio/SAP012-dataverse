13a84bc5b1fcb2c07abf0089eb9991bf3b04583c
deboraviveiros <deviveiros.debora@gmail.com>
2024-01-28 11:33:26 -0300

    Trabalhando com filtros, ordenação e visualização

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,5 +1,9 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
+//Preciso que as opções de filtro e organização apareçam em lista:
+// document.getElementById("organizer").innerHTML = data.
+// document.getElementById("filter").innerHTML = data.
+
 export const example = () => {
   return 'example';
 };
--- src/view.js
+++ src/view.js
@@ -1,10 +1,24 @@
 export const renderItems = (data) => {
   console.log(data)
-  // < div itemscope >
-  //   <ul>
-  //     <li></li>
-  //   </ul >
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
+  //Tentei declarar uma variável para que seja possível modificar a estrutura de <main>;
+  //const principal = document.querySelector("[class='main-container']");
+
+
+  //Tentei construir um código usando microdados para puxar as infos de dataset.js e colocar dentro do html, para que possam aparecer na página. Porém, ainda não entendi muito bem como fazer. Fia mais para entender a lógica.
+  document.getElementById("cards").innerHTML = data./*o que colocar aqui?*/.map((item /*declarar aqui o parâmetro, mas qual?*/) => `
+  <ul itemscope itemtype="https://schema.org/TouristTrip">
+    <li itemprop="itemListElement" itemscope itemtype: "https://schema.org/ListItem">
+    <a itemprop="item" href="./data/dataset.js">
+    <span itemprop="name"></span></a>
+    <meta itempro="position" content="3" />
+    </li>
+    `)
+
+  //É preciso construir uma forma de renderizar os objetos para que apareçam na página
+  /*< div itemscope >
+    <ul>
+      <li></li>
+    </ul >
+  Aquí comienza tu código y puedes retornar lo que tu necesites*/
   return 'example';
 };
-
